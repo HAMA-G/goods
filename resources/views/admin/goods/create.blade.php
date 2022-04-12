a89446bf1feed5e1140c607a98bfe18630ce8f59
KOKI <k-hamaji2525@gmail.com>
2022-04-12 00:47:40 +0000

    Auth finish

---
+++ resources/views/admin/goods/create.blade.php
@@ -0,0 +1,17 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'商品新規登録'を埋め込む --}}
+@section('title', 'グッズの新規登録')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>グッズの新規登録</h2>
+            </div>
+        </div>
+    </div>
+@endsection
